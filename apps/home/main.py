ee6a6208b87c14911ca7e484f28f0f485eebaed1
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-08-08 16:27:31 +0530

    added apps dir for sample apps

---
+++ apps/home/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render():
+    st.title("Welcome to the SAM: Streamlit App Manager")
+    st.caption("Coming Soon....")
---
+++ apps/test/src/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render():
+    st.title("App 1")
+    st.caption("Coming Soon....")
