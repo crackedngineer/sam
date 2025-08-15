405a955745f37f6ae3194762026c490e859d7a2b
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-08-15 16:55:51 +0530

    updated the apps path

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 APP_DIR_NAME = "apps"
-APP_DIR = Path(__file__).parent / APP_DIR_NAME
+APP_DIR = (Path(__file__).parent.parent / APP_DIR_NAME).resolve()
 DEFAULT_APP = "home"
