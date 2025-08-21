0405035e88bd0c0d78965fabfbb4dc22b1de18fb
Subhomoy Roy Choudhury <64763636+crackedngineer@users.noreply.github.com>
2025-08-21 10:05:11 +0000

    deleted home and test app

--- apps/home/main.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render():
-    st.title("Welcome to the SAM: Streamlit App Manager")
-    st.caption("Coming Soon....")
--- apps/test/src/app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render():
-    st.title("App 1")
-    st.caption("Coming Soon....")
